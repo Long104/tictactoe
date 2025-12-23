619f030cec0eda241c69563d2635edba552b6c7c
pantorn <longpantorn@gmail.com>
2025-12-23 14:18:34 +0700

    SCRUM-9 Fix table when insert inside

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ const Page = () => {
   return (
     <div className="w-svw h-svh flex justify-center items-center">
       <table className="border table-fixed w-[18rem] h-[18rem] sm:w-sm sm:h-[24rem] md:w-md md:h-[28rem] lg:w-lg lg:h-[32rem] xl:w-xl xl:h-[36rem] 2xl:w-2xl 2xl:h-[42rem]">
-        <tbody  className="[&>tr>td]:border">
+        <tbody className="[&>tr>td]:border">
           <tr>
             <td></td>
             <td></td>
