ce36c268eda5d3db8ae24ba5bad433951ffe053c
pantorn <longpantorn@gmail.com>
2025-12-23 14:21:59 +0700

    fix: table maximize the row that got any text inside the tr tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,19 @@ import react from "react";
 const Page = () => {
   return (
     <div className="w-svw h-svh flex justify-center items-center">
-      <table className="border table-fixed w-[18rem] h-[18rem] sm:w-sm sm:h-[24rem] md:w-md md:h-[28rem] lg:w-lg lg:h-[32rem] xl:w-xl xl:h-[36rem] 2xl:w-2xl 2xl:h-[42rem]">
-        <tbody className="[&>tr>td]:border">
-          <tr>
+      <table className="border table-fixed w-[18rem] h-[18rem] sm:w-[24rem] sm:h-[24rem] md:w-[28rem] md:h-[28rem] lg:w-[32rem] lg:h-[32rem] xl:w-[36rem] xl:h-[36rem] 2xl:w-[42rem] 2xl:h-[42rem]">
+        <tbody className="[&>tr>td]:border [&>tr>td]:text-center [&>tr>td]:align-middle">
+          <tr className="h-1/3">
+            <td>1</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr className="h-1/3">
             <td></td>
             <td></td>
             <td></td>
           </tr>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
+          <tr className="h-1/3">
             <td></td>
             <td></td>
             <td></td>
